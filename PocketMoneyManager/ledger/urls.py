da9e239b83e8af9392c5506a6cb7e608c486cdaf
ikrfun <takuma130625@keio.jp>
2021-12-02 10:30:41 +0900

    incomform url

--- PocketMoneyManager/ledger/urls.py
+++ PocketMoneyManager/ledger/urls.py
@@ -9,6 +9,6 @@ import ledger
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('list/', views.list.as_view(), name = 'list'),
-    path('pay_form/',views.pay_form,name = 'pay_form')
-    
+    path('pay_form/',views.pay_form,name = 'pay_form'),
+    path('incom_form/',views.pay_form,name = 'incom_form'),
 ]
