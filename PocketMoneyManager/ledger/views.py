4ca78b50541ca6a2899f1344be2bba134bf06439
ikrfun <takuma130625@keio.jp>
2022-01-07 17:07:22 +0900

    API 以外を削除　Nextjsなどでフロントを作る

--- PocketMoneyManager/ledger/views.py
+++
@@ -1,6 +0,0 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-def index(request):
-    return HttpResponse('Hello World')
